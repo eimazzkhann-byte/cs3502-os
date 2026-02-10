5f54addfbe8a41b308388f31ac1c41437bc01ef2
eimazzkhann-byte <eimazzkhann@gmail.com>
2026-02-10 11:26:18 -0500

    Update buffer.h
    remove line

--- assignment2/buffer.h
+++ assignment2/buffer.h
@@ -1,7 +1,9 @@
 // Name: Eimaz Khan
 // CS 3502 - Assignment 2
 
-/ ============================================
+cd ~/cs3502-os/assignment2
+nano buffer.h
+
 // buffer.h - Shared definitions (INCOMPLETE - You must complete this!)
 // ============================================
 #ifndef BUFFER_H
@@ -40,3 +42,4 @@
 
 
 #endif
+
