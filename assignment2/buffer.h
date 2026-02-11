d7aa56bb87009d6ad42379150801b44c1bf0c60d
eimazzkhann-byte <eimazzkhann@gmail.com>
2026-02-11 09:40:36 -0500

    Update buffer.h

--- assignment2/buffer.h
+++ assignment2/buffer.h
@@ -5,7 +5,6 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
-// Required includes for both producer and consumer
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -16,6 +15,7 @@
 #include <string.h>
 #include <signal.h>
 #include <time.h>
+#include <errno.h>
 
 // Constants for shared memory and semaphores
 #define BUFFER_SIZE 10
@@ -28,16 +28,17 @@
 
 // Item stored in the bounded buffer
 typedef struct {
-    int value;        // The data
-    int producer_id;  // Which producer created this item
+    int value;
+    int producer_id;
 } item_t;
 
 // Shared buffer structure in shared memory
 typedef struct {
+    int initialized;              // 0 = not initialized, 1 = initialized
     item_t buffer[BUFFER_SIZE];
-    int head;   // Next write position (producer)
-    int tail;   // Next read position (consumer)
-    int count;  // Current number of items
+    int head;                     // next write index
+    int tail;                     // next read index
+    int count;                    // number of items in buffer
 } shared_buffer_t;
 
 #endif
